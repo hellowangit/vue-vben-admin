fff9a14ebf6b375e34d7b7d60bd2f9eac826f1d7
hellowangit <hellowangit@gmail.com>
2022-04-15 16:02:00 +0800

    feat:  调整 prettier, 不使用分号

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   printWidth: 100,
-  semi: true,
+  semi: false,
   vueIndentScriptAndStyle: false,
   singleQuote: true,
   trailingComma: 'all',
   proseWrap: 'never',
   htmlWhitespaceSensitivity: 'strict',
   endOfLine: 'auto',
-};
+}
